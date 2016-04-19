98f6480d8f7b185d4e1afe099725ee6b81699806
Federico Caselli <CaselIT@users.noreply.github.com>
2016-04-19 08:37:58 +0200

    Fixed comment copy-paste typo

--- lock.d.ts
+++ lock.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Joint JS 0.9.7
+// Type definitions for RWLock 5.0.0
 // Project: https://github.com/71104/rwlock
 // Definitions by: Federico Caselli <https://github.com/CaselIT/typings-rwlock>
 
